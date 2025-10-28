c05d31b70287e8a482ece334c0a97860dc088d32
Emma <emma.deoliveira@ynov.com>
2025-10-28 11:10:51 +0100

    add action woodCutting(action.py)

--- game/actions/actions.py
+++ game/actions/actions.py
@@ -10,3 +10,8 @@ def sleep(adventurer):
     adventurer.energy = min(100, adventurer.energy + 20)
     adventurer.thirsty = max(0, adventurer.thirsty - 20)
     adventurer.hungry = max(0, adventurer.hungry - 20)
+
+def woodCutting(adventurer):
+    adventurer.energy = max(0, adventurer.energy - 20)
+    adventurer.thirsty = max(0, adventurer.thirsty - 20)
+    adventurer.hungry = max(0, adventurer.hungry - 20)
